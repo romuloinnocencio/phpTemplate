b32eb029f99eb2b6559e4e5c95604fa902062a13
Rômulo Innocêncio <romulodark@gmail.com>
2017-10-30 21:17:50 -0200

    Update header.php

--- modules/includes/header.php
+++ modules/includes/header.php
@@ -1,12 +1,24 @@
+<!-- header -->
 <header class="header" role="banner">
+    <div class="content cf">
 
-	<a href="/" title="my site" tabindex="1" accesskey="h">
-		<h1> my site </h1>
-		<img src="images/logo-mysite.svg">
-	</a>
-	
-	<nav class="header-navigation" role="navigation">	
-		
-	</nav>
-	
+		<!-- logotipo -->
+		<div class="header__logo">
+			<a href="/" title="..." tabindex="1" accesskey="h">
+				<h1> Meu site </span>
+				<img src="/images/....svg" alt="Logotipo Site"/>
+			</a>	    
+		</div>
+
+		<!-- menu mobile -->
+		<button id="js-menu-mobile" type="button">
+			<img src="/images/icons/icon-menu.svg" width="24" alt="ícone de menu"/>
+		</button>
+
+		<!-- nagecação principal -->
+		<nav class="header__nav" role="navigation" hidden>
+			<a class="header__nav--link" href="#link1" title="Link" tabindex="2" accesskey="2"> Link </a>
+		</nav>
+
+    </div>
 </header>
